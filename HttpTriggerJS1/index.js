08f66435e568c048a421d5266cf0cb872babe5d1
Carl Youngblood <carl@youngbloods.org>
2018-06-05 15:06:18 +0200

    Test app deployment

--- HttpTriggerJS1/index.js
+++ HttpTriggerJS1/index.js
@@ -4,7 +4,7 @@ module.exports = function (context, req) {
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
+            body: "Goodbye " + (req.query.name || req.body.name)
         };
     }
     else {
