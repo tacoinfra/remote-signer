abac1d0b8cd5f100110f74684d7fa41a75357c3b
Carl Youngblood <carl@youngbloods.org>
2018-06-05 23:44:16 +0200

    Check for byte sequence

--- lambda_function.py
+++ lambda_function.py
@@ -1,12 +1,24 @@
-import json
-
-print('Loading function')
+import base64
+import binascii
 
 
 def lambda_handler(event, context):
-    #print("Received event: " + json.dumps(event, indent=2))
-    print("value1 = " + event['key1'])
-    print("value2 = " + event['key2'])
-    print("value3 = " + event['key3'])
-    return event['key1']  # Echo back the first key value
-    #raise Exception('Something went wrong')
+    signature = ''
+    if 'to-sign' in event:
+        try:
+            decoded = base64.b64decode(event['to-sign'])
+        except binascii.Error:
+            raise Exception('Payload is not base64-encoded.') from binascii.Error
+        signature = decoded
+
+        bytes = list(decoded)
+        if bytes[0] != 0 and bytes[0] != 1:
+            raise Exception('Invalid preamble.')
+
+        # TODO: check block level is near current level
+        # TODO: check block hasn't already been signed
+
+    else:
+        raise Exception('Invalid payload.')
+
+    return {'signature': signature}
