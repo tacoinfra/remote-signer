59cccfb9501b957e8500e735e2a9ef27230dc69a
Carl Youngblood <carl@youngbloods.org>
2018-06-05 15:32:44 +0200

    Update function

--- HttpTriggerJS1/index.js
+++ HttpTriggerJS1/index.js
@@ -1,17 +1,21 @@
 module.exports = function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Goodbye " + (req.query.name || req.body.name)
-        };
+  if (req.query.name || (req.body && req.body.name)) {
+    var request = require('request');
+    request('http://www.google.com', function (error, response, body) {
+      context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: "Goodbye " + (req.query.name || req.body.name) + body;
+      };
+    });
+
     }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
-    context.done();
+  else {
+    context.res = {
+      status: 400,
+      body: "Please pass a name on the query string or in the request body"
+    };
+  }
+  context.done();
 };
