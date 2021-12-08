d979e2706b99c011b8f7d6162e21802f006ef328
Roland C. Dowdeswell <elric@imrryr.org>
2021-12-08 12:56:00 +0000

    Create class SignatureReq to encapsulate decoded the request
    We break much of the functionality of RemoteSigner out into
    a new class SignatureReq whose sole job is understanding the
    incoming payload.  This is in preparation for the tenderbake
    protocol updates where said decoding becomes slightly more
    complex.

--- src/remote_signer.py
+++ src/remote_signer.py
@@ -1,6 +1,5 @@
 
 import struct
-import string
 from pyhsm.hsmclient import HsmClient, HsmAttribute
 from pyhsm.hsmenums import HsmMech
 from pyhsm.convert import hex_to_bytes, bytes_to_hex
@@ -10,22 +9,17 @@ import bitcoin
 from pyblake2 import blake2b
 import logging
 
+from src.sigreq import SignatureReq
+
 
 class RemoteSigner:
-    BLOCK_PREAMBLE = 1
-    ENDORSEMENT_PREAMBLE = 2
     TEST_SIGNATURE = 'p2sigfqcE4b3NZwfmcoePgdFCvDgvUNa6DBp9h7SZ7wUE92cG3hQC76gfvistHBkFidj1Ymsi1ZcrNHrpEjPXQoQybAv6rRxke'
     P256_SIGNATURE = struct.unpack('>L', b'\x36\xF0\x2C\x34')[0]  # results in p2sig prefix when encoded with base58 (p2sig(98))
-    CHAIN_ID = struct.unpack('>L', b'\x00\x57\x52\x00')[0] # results in Net prefix when encoded with base58 (Net(15))
 
 
-    def __init__(self, config, payload='', ratchet=None):
+    def __init__(self, config, ratchet=None):
         self.keys = config['keys']
         self.ratchet = ratchet
-        self.payload = payload
-        logging.info('Verifying payload')
-        self.data = self.decode_block(self.payload)
-        logging.info('Payload {} is valid'.format(self.data))
         self.hsm_slot = config['hsm_slot']
         hsm_user = config['hsm_username']
         logging.info('HSM user is {}'.format(config['hsm_username']))
@@ -36,70 +30,25 @@ class RemoteSigner:
         logging.info('HSM lib is {}'.format(config['hsm_lib']))
         self.node_addr = config['node_addr']
 
-    @staticmethod
-    def valid_block_format(blockdata):
-        return all(c in string.hexdigits for c in blockdata)
-
-    @staticmethod
-    def decode_block(data):
-        return RemoteSigner.valid_block_format(data) and bytes.fromhex(data)
-
-    def is_block(self):
-        return self.data and list(self.data)[0] == self.BLOCK_PREAMBLE
-
-    def is_endorsement(self):
-        return list(self.data)[0] == self.ENDORSEMENT_PREAMBLE
-
-    def get_block_level(self):
-        level = -1
-        if self.is_block():
-            hex_level = self.payload[10:18]
-        else:
-            hex_level = self.payload[-8:]
-        level = struct.unpack('>L', unhexlify(hex_level))[0]
-        logging.info('Block level is {}'.format(level))
-        return level
-
-    def get_chain_id(self):
-        chainid = bytes.fromhex(self.payload[2:10])
-        return bitcoin.bin_to_b58check(chainid, magicbyte=RemoteSigner.CHAIN_ID)
-
-    def already_signed(self):
-        payload_level = self.get_block_level()
-        payload_chainid = self.get_chain_id()
-        if self.is_block():
-            sig_type = 'Baking_' + payload_chainid
-        else:
-            sig_type = 'Endorsement_' + payload_chainid
-        not_signed = self.ratchet.check(sig_type, payload_level)
+    def already_signed(self, sig_type):
+        not_signed = self.ratchet.check(sig_type, self.sigreq.get_level())
         if not_signed:
-            logging.info('{} signature for level {} has not been generated before'.format(sig_type, payload_level))
+            logging.info('{} signature for level {} has not been generated before'.format(sig_type, self.sigreq.get_level()))
         else:
-            logging.error('{} signature for level {} has already been generated!'.format(sig_type, payload_level))
+            logging.error('{} signature for level {} has already been generated!'.format(sig_type, self.sigreq.get_level()))
         return not not_signed
 
     @staticmethod
     def b58encode_signature(sig):
         return bitcoin.bin_to_b58check(sig, magicbyte=RemoteSigner.P256_SIGNATURE)
 
-    def sign(self, handle, test_mode=False):
-        payload_chainid = self.get_chain_id()
-        if self.is_block():
-            sig_type = 'Baking_' + payload_chainid
-        else:
-            sig_type = 'Endorsement_' + payload_chainid
+    def sign(self, handle, payload, test_mode=False):
+        logging.info('Verifying payload')
+        self.sigreq = SignatureReq(payload)
+        sig_type = f"{self.sigreq.get_type()}_{self.sigreq.get_chainid()}"
         encoded_sig = ''
-        data_to_sign = self.payload
-        logging.info('About to sign {} with key handle {}'.format(data_to_sign, handle))
-        if not self.valid_block_format(data_to_sign):
-            logging.error('Invalid payload')
-            raise Exception('Invalid payload')
-        logging.info('Block format is valid')
-        if not self.is_block() and not self.is_endorsement():
-            logging.error('Invalid preamble')
-            raise Exception('Invalid preamble')
-        logging.info('Preamble is valid')
-        if self.already_signed():
+        logging.info(f"About to sign {payload} with key handle {handle}")
+        if self.already_signed(sig_type):
             logging.error('Invalid level')
             raise Exception('Invalid level')
         if test_mode:
--- test/test_remote_signer.py
+++ test/test_remote_signer.py
@@ -1,49 +1,60 @@
 
+import re
+import struct
 import unittest
+
+from src.sigreq import SignatureReq
 from src.remote_signer import RemoteSigner
 from src.chainratchet import ChainRatchet, MockChainRatchet
 
 
+def eatwhite(str):
+    return re.sub(r"\s+", "", str)
+
+#
+# Here's a quick invalid block that we'll make sure that we don't process:
+
+INVALID_PREAMBLE = eatwhite("""
+        030000000000000002012c866bd675ad11475ea608dea4d9d166801f1725b2076363
+        63d55508aa07ba6f000000005b17b90d04683625c2445a4e9564bf710c5528fd99a7
+        d150d2a2a323bc22ff9e2710da4f6d00000011000000010000000008000000000000
+        000289b5a4e5e20c56512c64967dfa72e67c39166d5c48ad6884693c7d192e105c3b
+        00058f7b73557941607800""")
+
+#
+# Here we provide a list of valid blocks over which we shall iterate:
+# They are each a tuple of the expected results:
+#         (type, chainid, level)
+
+valid_sig_reqs = [
+      ("Baking", "NetXH12Aer3be93", 650, eatwhite("""
+        01000000000000028a0130009e2cb10ef25b54563989371653d9ba6545c475a1d583
+        ac8b4a28583df98d000000005b29ab5c04ce84d452f0c4accfbb23f42e5a23e91915
+        2a40ee17b56a6e7f1b95cfd20d792c00000011000000010000000008000000000000
+        2ceb3306c828b082cf23d15de02f6d5b028652569bf794e74d7c839c50c7e82fc781
+        0000e27015247d6713cd00"""))
+
+    , ("Endorsement", "NetXjkQyBJ9VYHe", 631, eatwhite("""
+        029feab277d4b686c59365261c4210f21d916fbb09f5e47e092a14b94e39fab61900
+        00000277"""))
+]
+
+
 class TestRemoteSigner(unittest.TestCase):
-    INVALID_PREAMBLE = '030000000000000002012c866bd675ad11475ea608dea4d9d166801f1725b207636363d55508aa07ba6f000000005b17b90d04683625c2445a4e9564bf710c5528fd99a7d150d2a2a323bc22ff9e2710da4f6d00000011000000010000000008000000000000000289b5a4e5e20c56512c64967dfa72e67c39166d5c48ad6884693c7d192e105c3b00058f7b73557941607800'
-    VALID_BLOCK = '01000000000000028a0130009e2cb10ef25b54563989371653d9ba6545c475a1d583ac8b4a28583df98d000000005b29ab5c04ce84d452f0c4accfbb23f42e5a23e919152a40ee17b56a6e7f1b95cfd20d792c000000110000000100000000080000000000002ceb3306c828b082cf23d15de02f6d5b028652569bf794e74d7c839c50c7e82fc7810000e27015247d6713cd00'
-    VALID_ENDORSEMENT = '029feab277d4b686c59365261c4210f21d916fbb09f5e47e092a14b94e39fab6190000000277'
-    SIGNED_BLOCK = 'p2sigfqcE4b3NZwfmcoePgdFCvDgvUNa6DBp9h7SZ7wUE92cG3hQC76gfvistHBkFidj1Ymsi1ZcrNHrpEjPXQoQybAv6rRxke'
-    PUBLIC_KEY = 'p2pk67jx4rEadFpbHdiPhsKxZ4KCoczLWqsEpNarWZ7WQ1SqKMf7JsS'
-    TEST_CONFIG = {
-        'hsm_username': 'resigner',
-        'hsm_slot': 1,
-        'hsm_lib': '/opt/cloudhsm/lib/libcloudhsm_pkcs11.so',
-        'node_addr': 'http://node.internal:8732',
-        'keys': {
-            'tz3aTaJ3d7Rh4yXpereo4yBm21xrs4bnzQvW': {
-                'public_key': 'p2pk67jx4rEadFpbHdiPhsKxZ4KCoczLWqsEpNarWZ7WQ1SqKMf7JsS',
-                'private_handle': 7,
-                'public_handle': 9
-            }
-        }
-    }
 
     def test_identifies_invalid_block_preamble(self):
-        rs = RemoteSigner(self.TEST_CONFIG, self.INVALID_PREAMBLE)
-        self.assertFalse(rs.is_block())
+        with self.assertRaises(Exception):
+            rs = RemoteSigner(self.TEST_CONFIG)
+            rs.sign(7, INVALID_PREAMBLE)
 
-    def test_identifies_valid_block_preamble(self):
-        rs = RemoteSigner(self.TEST_CONFIG, self.VALID_BLOCK)
-        self.assertTrue(rs.is_block())
-
-    def test_identifies_valid_endorsement_preamble(self):
-        rs = RemoteSigner(self.TEST_CONFIG, self.VALID_ENDORSEMENT)
-        self.assertTrue(rs.is_endorsement())
-
-    def test_decodes_block_level(self):
-        rs = RemoteSigner(self.TEST_CONFIG, self.VALID_BLOCK)
-        self.assertEqual(rs.get_block_level(), 650)
-
-    def test_signs_block(self):
-        rs = RemoteSigner(self.TEST_CONFIG, self.VALID_BLOCK,
-                          ratchet=MockChainRatchet())
-        self.assertEqual(rs.sign(7, test_mode=True), self.SIGNED_BLOCK)
+    def test_list_sigreqs(self):
+        for req in valid_sig_reqs:
+            #
+            # First we test if we are successfully parsing the blocks:
+            got = SignatureReq(req[3])
+            self.assertEqual(req[0], got.type)
+            self.assertEqual(req[1], got.chainid)
+            self.assertEqual(req[2], got.level)
 
 
 if __name__ == '__main__':
