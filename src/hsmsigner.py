8a2eaf2a9ffdfdbec4c6864cf9d861cd79cd20ec
Roland C. Dowdeswell <elric@imrryr.org>
2022-06-21 16:55:09 +0100

    our HSM C code isn't thread-safe, so we must mutex it

--- src/hsmsigner.py
+++ src/hsmsigner.py
@@ -3,6 +3,7 @@
 
 import logging
 from os import environ
+import threading
 
 from pyhsm.hsmclient import HsmClient
 from pyhsm.hsmenums import HsmMech
@@ -20,19 +21,21 @@ class HsmSigner(Signer):
         hsm_password = environ['HSM_PASSWORD']
         self.hsm_pin = f'{hsm_user}:{hsm_password}'
         self.hsm_libfile = config['hsm_lib']
+        self.lock = threading.Lock()
 
     def sign(self, handle, sigreq):
         logging.debug(f'Signing with HSM client:')
         logging.debug(f'    Slot = {self.hsm_slot}')
         logging.debug(f'    lib = {self.hsm_libfile}')
         logging.debug(f'    handle = {handle}')
-        with HsmClient(slot=self.hsm_slot, pin=self.hsm_pin,
-                       pkcs11_lib=self.hsm_libfile) as c:
-            hashed_data = blake2b(hex_to_bytes(sigreq.get_payload()),
-                                  digest_size=32).digest()
-            logging.debug(f'Hashed data to sign: {hashed_data}')
-            sig = c.sign(handle=handle, data=hashed_data,
-                         mechanism=HsmMech.ECDSA)
+        with self.lock:
+            with HsmClient(slot=self.hsm_slot, pin=self.hsm_pin,
+                           pkcs11_lib=self.hsm_libfile) as c:
+                hashed_data = blake2b(hex_to_bytes(sigreq.get_payload()),
+                                      digest_size=32).digest()
+                logging.debug(f'Hashed data to sign: {hashed_data}')
+                sig = c.sign(handle=handle, data=hashed_data,
+                             mechanism=HsmMech.ECDSA)
 
         logging.debug(f'Raw signature: {sig}')
         encoded_sig = Signer.b58encode_signature(sig)
