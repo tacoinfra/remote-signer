9ed798b5524db43ad71e50e869892bedd4bc8ad7
jyg007 <jeanyvesgirard@free.fr>
2022-11-14 05:10:59 +0530

    Support tz1 for grep11

--- src/hpcsgrep11signer.py
+++ src/hpcsgrep11signer.py
@@ -12,6 +12,8 @@ from pyblake2 import blake2b
 from src.signer import Signer
 import sys
 
+from pytezos_core.encoding import base58_encode
+
 class HPCSGrep11Signer(Signer):
     def __init__(self, config):
         self.config = config
@@ -26,22 +28,20 @@ class HPCSGrep11Signer(Signer):
         self.stub=g11grpc.CryptoStub(channel)
 
     def sign(self, pkh, sigreq):
-        
         sk=bytes.fromhex(self.config['keys'][pkh]['secret_key'])
         hashed_data = blake2b(bytes.fromhex(sigreq.get_payload()), digest_size=32).digest()
         logging.debug(f'Hashed data to sign: {hashed_data}')
 
-        if phk[2] == "3":
-            CKM_ECDSA = 0x00001041
-            mech=g11.Mechanism(Mechanism=CKM_ECDSA)
-        elif phk[2] == 1:
-            CKM_IBM_ED25519_SHA512 = 0x8001001c
-            mech=g11.Mechanism(Mechanism=CKM_IBM_ED25519_SHA512)
-        
+        ckm={"3":0x00001041 , "1":0x8001001c}  # CKM_ECDSA et CKM_IBM_ED25519_SHA512
+        mech=g11.Mechanism(Mechanism=ckm[pkh[2]])
+#        d={"3": hashed_data, "1": bytes.fromhex(sigreq.get_payload()) } 
+#        request = g11.SignSingleRequest(Mech=mech, PrivKey=sk, Data=d[pkh[2]])
         request = g11.SignSingleRequest(Mech=mech, PrivKey=sk, Data=hashed_data)
 
+        p={"3": b'p2sig', "1": b'edsig' } 
         response = self.stub.SignSingle(request)
         logging.debug(f'Raw signature: {response.Signature}')
-        encoded_sig = Signer.b58encode_signature(response.Signature)
+#        encoded_sig = Signer.b58encode_signature(response.Signature)
+        encoded_sig = base58_encode(response.Signature, prefix=p[pkh[2]])
         logging.debug(f'Base58-encoded signature: {encoded_sig}')
         return encoded_sig
