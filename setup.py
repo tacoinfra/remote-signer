556c32839af0d9e03365329f5c48979791a0adff
Roland C. Dowdeswell <elric@imrryr.org>
2023-11-17 15:20:19 +0000

    Ref requirements.txt from setup.py to get them into the pkg

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='tezos-signer',
     version='0.9.9',
     packages=['tezos_signer'],
-    scripts=['scripts/signer', 'scripts/start-remote-signer']
+    scripts=['scripts/signer', 'scripts/setup-al2023'],
+    install_requires=requirements,
 )
